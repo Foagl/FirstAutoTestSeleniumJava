3fe357e03f2c54048e42383c5790e59802495ce2
Valeriy <ValeriySlykin@gmail.com>
2021-01-15 18:30:06 +0200

    Initial commit

---
+++ src/test/java/AutoTest.java
@@ -0,0 +1,67 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+// Для доступа к элементам внутри DOM используются [] скобки.
+public class AutoTest {
+    public ChromeDriver driver;
+
+    @Before
+ public void SetUp(){
+        driver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+        System.out.println("open");
+    }
+    @After
+public void SetClose(){
+        //driver.close();
+        System.out.println("close");
+    }
+    @Test
+    public void singUp(){
+         driver.get("https://wp-seven.ru/");
+         WebElement header = driver.findElementById("headr");
+         header.findElement(By.cssSelector("[href=\"/registration/\"]")).click();
+        WebDriverWait wait = new WebDriverWait(driver,15);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("signup_submit")));
+        driver.findElement(By.id("signup_username")).sendKeys("foagl2021");
+        driver.findElement(By.id("signup_email")).sendKeys("valeriyslykin@ukr.net");
+        driver.findElement(By.id("signup_password")).sendKeys("Foagl2020");
+        driver.findElement(By.id("signup_password_confirm")).sendKeys("Foagl2020");
+        driver.findElement(By.id("field_1")).sendKeys("Valerii");
+        driver.findElement(By.xpath("//*[@id=\"basic-details-section\"]/div[5]/label[1]")).click(); // Нажатие radio что я не бот ;)
+        driver.findElement(By.id("signup_submit")).click();
+        String str = driver.findElement(By.xpath("//*[@id=\"basic-details-section\"]/div[7]/div")).getText();
+        // строку получил.
+        String strAnswer = str.replaceAll("[0-9=?]", "");
+        String news = new String(strAnswer);
+        // создаю новую без цифр из равно вопрос.
+        str = str.replaceAll("[^\\d.]", "");
+        // удалишнее кроме цифр
+
+        int i=Integer.parseInt(str);
+        int j = i / 10;
+        int k = i - (j * 10);
+        if (news.equals("-")) System.out.println(" 51 " + (i - j));
+        if (news.equals("+")) System.out.println(" 52 " + (i + j));
+        String botStr = String.valueOf(k);
+        System.out.println(str);
+        System.out.println(j);
+        System.out.println(k);
+        System.out.println("news = " + news);
+
+        driver.findElement(By.xpath("//*[@id=\"basic-details-section\"]/div[7]/input")).sendKeys(botStr);
+        //driver.findElement(By.id("signup_submit")).click();
+
+
+    }
+}
